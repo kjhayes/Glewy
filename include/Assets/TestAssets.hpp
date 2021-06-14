e7e06673c8c88c3a1938c902d1dd3b2fb4212e64
Kevin Hayes <kjhayes47@yahoo.com>
2021-06-14 14:55:18 -0400

    Reworked Bin Folder

--- include/Assets/TestAssets.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ASSET_TestAssets
-#define ASSET_TestAssets
-namespace gly{class Data;}
-class TestAssets{
-public:
-static gly::Data BlobSpikeTable;
-static gly::Data BlobSpikeTex;
-};
-#endif
