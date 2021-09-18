ab9cceadfe9e1163c2a8e86221d9e11d84c1cf99
Kevin Hayes <kjhayes47@yahoo.com>
2021-09-18 10:47:24 -0400

    Further Dependencies Support

--- depend/CLONE_DEP_GIT.bat
+++ depend/CLONE_DEP_GIT.bat
@@ -1,4 +1,4 @@
-git clone https://github.com/edoren/glew.git
+git clone https://github.com/Perlmint/glew-cmake.git
 git clone https://github.com/glfw/glfw.git
 git clone https://github.com/paralin/soil.git
 git clone https://github.com/kjhayes/SoLoud_MA.git
